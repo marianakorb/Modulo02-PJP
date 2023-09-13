7cde5a18e6306b2d5fa045c2267d49a3152a7fc0
mariana <mariana.korb.mk@gmail.com>
2023-09-13 20:06:58 -0300

    dom

--- Front-end/exercicios/assents/scripts/script.js
+++ Front-end/exercicios/assents/scripts/script.js
@@ -4,8 +4,13 @@ criarElementoHTML = () => {
 
     // <div id="meuDiv" class="minhaClasse"></div>
     novoElemento.id = 'meuDiv';
-    novoElemento.className = 'minhaClasse';
 
+    const valor = Math.floor(Math.random() * 100) + 1;
+    console.log(valor);
+
+    if(valor <= 70 ) {
+        novoElemento.className = "minhaClasse";
+    }
     // texto para ser inserido em um nó do DOM HTML
     const texto = document.createTextNode("Esse é o texto que vou colocar no elemento");
 
@@ -15,6 +20,20 @@ criarElementoHTML = () => {
 }
 
 aplicarClassBotao = () => {
-    
+    const botoes = document.querySelectorAll("button");
+
+    botoes.forEach(function(btn) {
+        btn.style.color = "blue";
+        btn.style.backgroundColor = "#fff";
+        btn.style.borderRadius = "10px";
+        btn.style.padding = "5px";
+    })
 }
 
+alterarClassElement() = () => {
+    //document.querySelectorAll("#meuDiv").classList.add("padrao");
+
+    document.querySelectorAll("meuDiv").forEach(function(el) {
+        el.classList.add("padrao");
+    })
+}
