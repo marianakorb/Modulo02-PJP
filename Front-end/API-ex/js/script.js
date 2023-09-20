a38ac10e22a86ead010d7d764dfddd62a2687b1c
mariana <mariana.korb.mk@gmail.com>
2023-09-20 20:24:28 -0300

    API estados e cidades

---
+++ Front-end/API-ex/js/script.js
@@ -0,0 +1,52 @@
+const loadCidade = async (uf) => {
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/distritos?orderBy=nome`;
+
+    const optionCidade = document.getElementsByClassName("option-cidade");
+
+    while(optionCidade.length != 0) {
+        optionCidade[0].remove();
+    } 
+
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json(); 
+        
+        const element = document.getElementById('cidade');
+        let option = "";
+        for(let cidade of data) {
+            option += `<option class="option-cidade" value=${cidade.nome} id=${cidade.id}>${cidade.nome}</option>`;
+        }
+
+        element.insertAdjacentHTML("beforeend", option);
+
+    } catch (error) {
+        alert("Ops! Algo deu errado na carga dos municípios da união federativa " + uf);
+    }
+}
+
+// async - avisa que tenho uma promessa (callback)
+const loadEstado = async () => {
+    const url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome";
+
+    try {
+        // await - aguarda processar essa linha para depois ir para a próxima
+        const response = await fetch(url);
+        const data = await response.json();
+
+        const element = document.getElementById('estado');
+        let option = "";
+
+        for(let estado of data) {
+            option += `<option value=${estado.sigla} id=${estado.id}>${estado.nome}</option>`;
+        }
+
+        element.insertAdjacentHTML("beforeend", option);
+
+    } catch (error) {
+        alert("Ops! Algo deu errado.");
+    }
+}
+
+
+loadEstado();
