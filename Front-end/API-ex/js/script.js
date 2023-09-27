8a45c72154f0b914b7991bad2bac2993b154a142
mariana <mariana.korb.mk@gmail.com>
2023-09-27 15:21:54 -0300

    aprimorado

--- Front-end/API-ex/js/script.js
+++ Front-end/API-ex/js/script.js
@@ -1,3 +1,70 @@
+const loadCidade = (uf) =>{
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/distritos?orderBy=nome`
+    load(url, 'cidade');
+    resultado('estado');
+}
+const loadEstado = () =>{
+    const url = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome';
+   load(url, 'estado');
+}
+
+const resultado = (action) =>{
+    const element = document.getElementById(action);
+    document.getElementById('item-cidade').textContent = '';
+    
+    if(action == 'estado'){
+        const estado = element.options[element.selectedIndex].text;
+        document.getElementById('item-estado').textContent = estado;
+        aplicarDisplay('resultado','none');
+    }
+    if(action =='cidade'){
+        const cidade = element.options[element.selectedIndex].text;
+        document.getElementById('item-cidade').textContent = cidade;    
+        aplicarDisplay('resultado','block');
+    }
+}
+
+const aplicarDisplay = (id, action) =>{
+    document.getElementById(id).style.display = action;
+}
+
+const load = async (url, action) =>{
+    try {        
+        const response = await fetch(url);
+        const data = await response.json();
+        const element = document.getElementById(action);
+        let option = '';
+
+        switch (action) {
+            case 'cidade':
+                const optionCidade = document.getElementsByClassName('option-cidade');
+                while(optionCidade.length != 0){
+                    optionCidade[0].remove();
+                }
+                for(let cidade of data ){
+                    option += `<option class='option-cidade' value=${cidade.nome} id=${cidade.id}>${cidade.nome}</option>`;
+                }
+                break;
+            case 'estado':
+                for(let estado of data){
+                    if(estado.id != 53){
+                        option += `<option value=${estado.sigla} id=${estado.id}> ${estado.nome} </option>`;
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+        element.insertAdjacentHTML("beforeend",option);
+
+    } catch (error) {
+        alert("Ops! algo deu errado");
+    }
+}
+
+loadEstado();
+
+/*
 const loadCidade = async (uf) => {
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${uf}/distritos?orderBy=nome`;
 
@@ -48,5 +115,4 @@ const loadEstado = async () => {
     }
 }
 
-
-loadEstado();
+loadEstado(); */
